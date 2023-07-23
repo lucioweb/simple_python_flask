ef1f2cab25eeb8fa4d2db3841d9ba1219aade207
lucioweb <lucio.lemos.385@ufrn.edu.br>
2023-07-23 20:52:45 -0300

    commit

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Olá! Bem vindo ao seu Projeto Simple_Python_Flask</p>"
